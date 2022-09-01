a1567a135aa82c6a557a654c1d05ad5fc4cccfab
gmanasses <guilhermemanasses98@gmail.com>
2022-08-31 22:18:30 -0300

    Simple game to learn the basics of libGDX

---
+++ desktop/src/com/duckhat/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.duckhat.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.duckhat.game.MyFirstGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("My First GDX Game");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+
+		new Lwjgl3Application(new MyFirstGdxGame(), config);
+	}
+}
